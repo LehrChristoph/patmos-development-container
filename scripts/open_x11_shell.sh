6d7a30176d4d64c959910decbe28b69ad94b266b
christoph <christoph.lehr@gmx.at>
2021-10-05 11:28:49 +0200

    updated naming

--- scripts/open_x11_shell.sh
+++ scripts/open_x11_shell.sh
@@ -24,7 +24,7 @@ if  [ -z "${j}" ] || [ -z "${s}" ] || [ -z "${p}" ]; then
     usage
 fi
 
-podman run --rm -it --name patmos-development-container -v ${s}:/dev/ttyUSB0 -v ${j}:/dev/jtag --network host --privileged \
+podman run --rm -it --name patmos-x11-container -v ${s}:/dev/ttyUSB0 -v ${j}:/dev/jtag --network host --privileged \
 	-v ${p}:/opt/t-crest/patmos  --workdir /opt/t-crest/patmos --group-add keep-groups --ipc host \
 	--ipc host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
 	docker.io/lehrchristoph/patmos-dev-container:v1 
